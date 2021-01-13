8e4486fa6a18b3620a66d3916f65b13043f1e741
github username <github email address>
2021-01-12 20:34:26 -0500

    form input

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let form=document.querySelector("form")
+    form.addEventListener("submit",function(event){
+        console.log(event)
+    })
+})
